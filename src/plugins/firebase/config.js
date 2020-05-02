3861bf2bf6e51aafc204aefe7b384aad86136aaa
dmgame <denis.m.pcspace@gmail.com>
2020-05-02 21:11:16 +0300

    base commit

---
+++ src/plugins/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBWeOt1--vbtlZxg61VkFd3hBryzYnRnq4',
+  authDomain: 'chat-app-e4bf8.firebaseapp.com',
+  databaseURL: 'https://chat-app-e4bf8.firebaseio.com',
+  projectId: 'chat-app-e4bf8',
+  storageBucket: 'chat-app-e4bf8.appspot.com',
+  messagingSenderId: '1013833641127',
+  appId: '1:1013833641127:web:b163869d4469be06857f82',
+};
+
+export default firebaseConfig;
